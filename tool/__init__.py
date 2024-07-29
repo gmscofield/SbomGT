6e0de7aeb1382bd4750bf7fefb879230a9a6c30a
gmscofield <gmscofield123@gmail.com>
2024-07-29 10:23:57 +0800

    SbomGT

---
+++ tool/__init__.py
@@ -0,0 +1,3 @@
+py_env = ["anaconda3", "conda3", "anaconda", "conda", "miniconda", "miniconda3", "pip3", "pip", "pypi", "pdm", "poetry", "pipfile"]
+
+# sys.path.append("../../../")
---
+++ tool/__main__.py
@@ -0,0 +1,11 @@
+# from .meta.conda import analyze_CondaMeta
+# from .meta.pypi import analyze_PyprojectMeta
+# from .analyzeSbom import buildBom
+
+
+# bom = buildBom("/root/project/SbomGT/tool/example/cyclonedx-python")
+# bom.makeBOM("/root/project/test/", "txt")
+# buildBom("/root/project/SbomGT/tool/example/cyclonedx-python", 3)
+
+# result = analyze_PyprojectMeta("/root/project/SbomGT/tool/example/pyproject.toml", 3)
+# print(result)
